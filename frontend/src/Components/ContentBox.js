c296e9b847dec6be2342a57efd237a513e4c37d8
n-zad <bayati@calpoly.edu>
2022-03-04 15:31:39 -0800

    cleaned up ContentBox.js

--- frontend/src/Components/ContentBox.js
+++ frontend/src/Components/ContentBox.js
@@ -4,10 +4,10 @@ import Box from "@mui/material/Box";
 import axios from 'axios';
 import { postList } from "./Post/post";
 
-export default function ContentBox(props) {
+export default function ContentBox() {
 	const [state, setState] = useState({
 		posts: []
-	  })
+	})
 	
 	useEffect(() => {
 		axios.get(`http://localhost:3030/post/`)
@@ -19,15 +19,15 @@ export default function ContentBox(props) {
 	},[]);
 	
 	return (
-			<Router>
-				<Box sx={{ flexGrow: 1 }} style={{marginLeft: 100, marginTop: 30, marginRight: 50, marginBottom: 30}}>
-					<Routes>
-						<Route exact path="/" element={postList("", state.posts)} />
-						<Route exact path="/popular" element={postList("popular", state.posts)} />
-						<Route exact path="/recent" element={postList("recent", state.posts)} />
-						<Route exact path="/trending" element={postList("trending", state.posts)} />
-					</Routes>
-				</Box>
-			</Router>
-		);
+		<Router>
+			<Box sx={{ flexGrow: 1 }} style={{marginLeft: 100, marginTop: 30, marginRight: 50, marginBottom: 30}}>
+				<Routes>
+					<Route exact path="/" element={postList("", state.posts)} />
+					<Route exact path="/popular" element={postList("popular", state.posts)} />
+					<Route exact path="/recent" element={postList("recent", state.posts)} />
+					<Route exact path="/trending" element={postList("trending", state.posts)} />
+				</Routes>
+			</Box>
+		</Router>
+	);
 }
