c53511097909c5c16fc879c65e87227d3bcb3591
phu <phu@phus-MacBook-Pro.local>
2022-05-02 15:58:28 -0700

    test build

--- backend/models/DatabaseHandler.js
+++ backend/models/DatabaseHandler.js
@@ -15,7 +15,7 @@ function getDbConnection() {
   const databaseJest =
     "mongodb+srv://msultano:4152838823OrifSs@cluster0.az8az.mongodb.net/CSC308?retryWrites=true&w=majority";
   if (!dbConnection) {
-    dbConnection = mongoose.connect(databaseJest || database, {
+    dbConnection = mongoose.connect(databaseJest, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
